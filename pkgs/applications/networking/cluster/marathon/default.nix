3f24c836db026b91f2c2a2e457a5944afb90f14b
Azul <mail@azulinho.com>
2017-01-19 16:39:11 +0000

    marathon: update to 1.3.6

--- pkgs/applications/networking/cluster/marathon/default.nix
+++ pkgs/applications/networking/cluster/marathon/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "marathon-${version}";
-  version = "1.1.5";
+  version = "1.3.6";
 
   src = fetchurl {
     url = "https://downloads.mesosphere.com/marathon/v${version}/marathon-${version}.tgz";
-    sha256 = "14w4p751mkhf6bv74qb8az3xmsns0aj3sxj75ibaq92n71xzqxpd";
+    sha256 = "12a6ah6qsx1ap6y7sps4vwkq8lyc08k1qnak2mnsa04ifrx9z0dy";
   };
 
   buildInputs = [ makeWrapper jdk mesos ];
