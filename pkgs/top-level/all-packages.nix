jamagin/nixpkgs

pythonPackages.ansible2: move 2.2 to separate file, make default
`pythonPackages.ansible_2_2` is now the default `ansible`.

pkgs/top-level/all-packages.nix
@@ -6007,8 +6007,7 @@ with pkgs;
 
   augeas = callPackage ../tools/system/augeas { };
 
-  ansible = python2Packages.ansible;
-
+  ansible  = python2Packages.ansible2;
   ansible2 = python2Packages.ansible2;
 
   antlr = callPackage ../development/tools/parsing/antlr/2.7.7.nix { };

Azulinho/mynixpkgs

pkgs/top-level/all-packages.nix
@@ -6101,7 +6101,7 @@ in
 
   augeas = callPackage ../tools/system/augeas { };
 
-  ansible = pythonPackages.ansible;
+  ansible = pythonPackages.ansible2;
 
   ansible2 = pythonPackages.ansible2;
 