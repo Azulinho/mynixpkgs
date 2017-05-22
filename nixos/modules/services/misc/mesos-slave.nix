1630cb8538f7cfe59efa17acfc57932121aab235
Azul <mail@azulinho.com>
2017-05-22 16:43:22 +0100

    keep the same mesos-slave boot_id

--- nixos/modules/services/misc/mesos-slave.nix
+++ nixos/modules/services/misc/mesos-slave.nix
@@ -226,6 +226,8 @@ in {
       preStart = ''
         mkdir -m 0700 -p ${cfg.workDir}
         chown ${cfg.mesosUser} ${cfg.workDir}
+        # make sure mesos-slave on restart won't overwrite the boot_id
+        test -e ${cfg.workDir}/meta/boot_id && chmod +i ${cfg.workDir}/meta/boot_id
       '';
     };
   };
